1ac9f930545ef0bb0dc691997e79c84e2fb5a646
Egno <ex.lxndr@gmail.com>
2019-04-22 13:57:16 +0700

    nice selected date

--- src/components/SelectedTime.vue
+++ src/components/SelectedTime.vue
@@ -1,12 +1,30 @@
 <template>
-  <v-layout row>
-    <v-flex v-if="date" xs6>
-      <div>{{ date }}</div>
-    </v-flex>
-    <v-flex v-if="time" xs6>
-      <div>{{ time }}</div>
-    </v-flex>
-  </v-layout>
+  <v-card flat class="round">
+    <v-card-title>
+      <v-layout column fill-height align-start justify-center>
+        <v-flex xs-12>
+          <v-layout row>
+            <v-flex v-if="date" xs-6>
+              <h3>{{ displayDate }},</h3>
+            </v-flex>
+            <v-flex v-if="time" xs-6>
+              <h3>{{ time }}</h3>
+            </v-flex>
+          </v-layout>
+        </v-flex>
+      </v-layout>
+      
+      <v-spacer />
+      <v-btn
+        flat
+        icon
+        color="#dee1e9"
+        @click="onRemove()"
+      >
+        <v-icon>delete</v-icon>
+      </v-btn>
+    </v-card-title>
+  </v-card>
 </template>
 
 <script>
@@ -14,10 +32,15 @@ import { mapGetters } from "vuex"
 
 export default {
   computed: {
-    ...mapGetters(['date', 'time'])
-  },
-  methods: {
-//
+    ...mapGetters(['date', 'time']),
+    displayDate () {
+      let d = new Date(this.date)
+      return d.toLocaleString('ru', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+      }).replace(' г.','')
+    }
   }
 }
 </script>
