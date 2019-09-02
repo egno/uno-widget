f14d2311aa94cab9d2eabf5595b164b5edafc26a
Egno <ex.lxndr@gmail.com>
2019-09-02 16:44:18 +0700

    mod service selector

--- src/components/ServicePage.vue
+++ src/components/ServicePage.vue
@@ -1,72 +1,69 @@
 <template>
-  <v-layout column justify-space-between class="max-height">
-    <div>
-      <v-flex>
-        <h2>Выберите услугу</h2>
-      </v-flex>
-      <v-flex>
-        <v-text-field v-model="searchString" prepend-inner-icon="search" placeholder="Поиск" />
-      </v-flex>
-      <v-flex>
-        <v-layout column>
-          <template v-if="progress">
-            <div class="text-xs-center">
-              <v-progress-circular indeterminate color="primary" />
-            </div>
-          </template>
-          <template v-else>
-            <v-flex v-for="group in groups" :key="'grp'+group">
-              <v-layout column>
-                <v-flex>
-                  <span class="subheading">{{ group || 'Прочее' }}</span>
-                </v-flex>
-                <v-flex>
-                  <v-layout column>
-                    <v-flex
-                      v-for="service in servicesInGroup(group)"
-                      :key="'srv'+service.service.id"
-                      class="card-item"
-                    >
-                      <ServiceCard :service="service" :group="groupInfo(group)" />
-                    </v-flex>
-                  </v-layout>
-                </v-flex>
-              </v-layout>
-            </v-flex>
-          </template>
-        </v-layout>
-      </v-flex>
-    </div>
-    <div>
-      <v-flex v-if="servicesCount">
-        <v-card flat color="#f3f4f7">
-          <SelectedServices color="grey" />
-          <v-toolbar flat>
-            <v-toolbar-title flat>
+  <div>
+    <div class="pb-2">
+      <v-layout column justify-space-between>
+        <v-flex>
+          <h2>Выберите услугу</h2>
+        </v-flex>
+        <v-flex>
+          <v-text-field v-model="searchString" prepend-inner-icon="search" placeholder="Поиск" />
+        </v-flex>
+        <v-flex>
+          <v-card v-if="servicesCount" flat color="#f3f4f7" class="rounded">
+            <SelectedServices color="grey" />
+            <ButtonToolbar @click="onNext">
               <v-layout row>
-                <v-flex xs-6 class="body-1">
+                <v-flex xs-6 class="body-1" pr-3>
                   <DurationDisplay :value="+duration" />
                 </v-flex>
                 <v-flex xs-6 class="body-1">
                   <PriceDisplay :value="+price" />
                 </v-flex>
               </v-layout>
-            </v-toolbar-title>
-            <v-spacer />
-            <v-btn outline fab small ligth color="#ddd" class="toolbar-button" @click="onNext">
-              <v-icon color="black">
-                arrow_forward
-              </v-icon>
-            </v-btn>
-          </v-toolbar>
-        </v-card>
-      </v-flex>
+            </ButtonToolbar>
+          </v-card>
+        </v-flex>
+      </v-layout>
     </div>
-  </v-layout>
+    <div>
+      <v-layout column justify-space-between class="max-height">
+        <v-flex>
+          <v-layout column>
+            <template v-if="progress">
+              <div class="text-xs-center">
+                <v-progress-circular indeterminate color="primary" />
+              </div>
+            </template>
+            <template v-else>
+              <v-flex v-for="group in groups" :key="'grp'+group">
+                <v-layout column>
+                  <v-flex>
+                    <span class="subheading">{{ group || 'Прочее' }}</span>
+                  </v-flex>
+                  <v-flex>
+                    <v-layout column>
+                      <v-flex
+                        v-for="service in servicesInGroup(group)"
+                        :key="'srv'+service.service.id"
+                        class="card-item"
+                      >
+                        <ServiceCard :service="service" :group="groupInfo(group)" />
+                      </v-flex>
+                    </v-layout>
+                  </v-flex>
+                </v-layout>
+              </v-flex>
+            </template>
+          </v-layout>
+        </v-flex>
+      </v-layout>
+    </div>
+  </div>
 </template>
 
 <script>
 import Api from "@/api/backend"
+import ButtonToolbar from "@/components/ButtonToolbar.vue"
 import ServiceCard from "@/components/ServiceCard.vue"
 import SelectedServices from "@/components/SelectedServices.vue"
 import DurationDisplay from "@/components/DurationDisplay.vue"
@@ -74,13 +71,20 @@ import PriceDisplay from "@/components/PriceDisplay.vue"
 import { mapActions, mapGetters } from "vuex"
 
 export default {
-  components: { SelectedServices, ServiceCard, DurationDisplay, PriceDisplay },
+  components: {
+    ButtonToolbar,
+    SelectedServices,
+    ServiceCard,
+    DurationDisplay,
+    PriceDisplay
+  },
   data () {
     return {
       allServices: [],
       searchString: "",
       serviceGroups: [],
-      progress: false
+      progress: false,
+      snackbar: false
     }
   },
   computed: {
@@ -111,7 +115,8 @@ export default {
     }
   },
   watch: {
-    filialId: "load"
+    filialId: "load",
+    servicesCount: "showTotals"
   },
   mounted () {
     this.loadGroupsInfo()
@@ -160,6 +165,11 @@ export default {
     },
     onNext () {
       this.setStep("main")
+    },
+    showTotals () {
+      setTimeout(() => {
+        window.scrollTo(0, 0)
+      }, 1000)
     },
     servicesInGroup (grp) {
       return this.filteredServices.filter(x => x.service.group === grp)
